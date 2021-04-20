0e42cd159a4dd2762f3acc91941a6b1d13eb7d00
claudio <jeff.797886@gmail.com>
2021-04-21 00:00:18 +0800

    Initial Commit

---
+++ gesture/gestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestureApp.swift
+//  gesture
+//
+//  Created by 張凱翔 on 2021/4/21.
+//
+
+import SwiftUI
+
+@main
+struct gestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
